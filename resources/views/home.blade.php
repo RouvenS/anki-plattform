7391a654fdcb6f6fdfadc965091241414da65e9e
Rouven Schuessler <rouven.schuessler@outlook.com>
2025-10-22 22:21:06 +0200

    wip: prompt page icons2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -114,7 +114,9 @@ try - пытаться"
                             <form class="inline-block" action="{{ route('batches.destroy', $batch) }}" method="POST" onsubmit="return confirm('Are you sure?');">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="btn-danger">Delete</button>
+                                <button type="submit" class="btn-icon btn-icon-danger">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" /></svg>
+                                </button>
                             </form>
                         </div>
                     </div>
@@ -148,7 +150,10 @@ try - пытаться"
                                 <form class="inline-block" action="{{ route('batches.destroy', $batch) }}" method="POST" onsubmit="return confirm('Are you sure?');">
                                     @csrf
                                     @method('DELETE')
-                                    <button type="submit" class="btn-danger">Delete</button>
+                                    <button type="submit" class="btn-danger">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" /></svg>
+                                        <span>Delete</span>
+                                    </button>
                                 </form>
                             </td>
                         </tr>
